4bf749e236ff8b51085d9d8277fbb5594875e852
TomoyukiZaitsu <zaitsutomoyuki@gmail.com>
2025-05-19 01:24:35 +0900

    5/19 作業分

--- src/main/java/com/booknest/auth/entity/UserEntity.java
+++ src/main/java/com/booknest/auth/entity/UserEntity.java
@@ -2,6 +2,8 @@ package com.booknest.auth.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,35 +21,42 @@ public class UserEntity {
 	/**
 	 * ユーザーID
 	 */
+	@Column(name = "user_id")
 	private Long userId;
 
 	/**
 	 * email
 	 */
+	@Column(name = "email")
 	private String email;
 
 	/**
 	 * パスワード
 	 */
+	@Column(name = "password")
 	private String password;
 
 	/**
 	 * 姓
 	 */
+	@Column(name = "first_name")
 	private String firstName;
 
 	/**
 	 * 名
 	 */
+	@Column(name = "last_name")
 	private String lastName;
 
 	/**
 	 * 登録日時
 	 */
-	private final LocalDateTime createdAt = LocalDateTime.now();
+	@Column(name = "created_at")
+	private final LocalDateTime createdAt;
 
 	/**
 	 * 更新日時
 	 */
+	@Column(name = "update_at")
 	private LocalDateTime updatedAt;
 }
